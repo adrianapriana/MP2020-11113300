b8deadc8f83efb4e3c9658440e4fc0b4bbe153f0
adrianapriana <11113300@student.unklab.ac.id>
2020-07-06 18:15:26 +0800

    Submit Pertemuan 18

---
+++ pertemuan18/Composition/src/com/multiplatform/Case.java
@@ -0,0 +1,29 @@
+package com.multiplatform;
+
+public class Case {
+    private String model;
+    private String manufacturer;
+    private String powerSupply;
+
+    public Case(String model, String manufacturer, String powerSupply) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.powerSupply = powerSupply;
+    }
+
+    public void pressPowerButton(){
+        System.out.println("Power Button is pressed...");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getPowerSupply() {
+        return powerSupply;
+    }
+}
